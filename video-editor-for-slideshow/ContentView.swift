5541f0817624e6f22e20101ea91cbabfc65bbb64
asifhasan002 <asifannada@gmail.com>
2026-01-08 14:43:38 +0600

    initial commit

---
+++ video-editor-for-slideshow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  video-editor-for-slideshow
+//
+//  Created by Md Asif Hasan on 8/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ video-editor-for-slideshow/video_editor_for_slideshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_editor_for_slideshowApp.swift
+//  video-editor-for-slideshow
+//
+//  Created by Md Asif Hasan on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct video_editor_for_slideshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
